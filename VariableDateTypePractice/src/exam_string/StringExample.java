345cf35121e35eb0c777f550606f395b887aaa5b
gustmdakstp <hs8206@naver.com>
2026-04-29 15:09:11 +0900

    연습용1

---
+++ VariableDateTypePractice/src/exam_string/StringExample.java
@@ -0,0 +1,27 @@
+package exam_string;
+
+public class StringExample {
+        static void main(String[] args) {
+              String name = "홍길동";
+              String job = "프로그래머";
+              System.out.println(name);
+              System.out.println(job);
+
+              String str = "나는 \"자바\"를 배웁니다.";
+              System.out.println(str);
+
+              str = "번호\t이름\t직업 ";
+              System.out.println(str);
+
+              System.out.print("나는\n");
+              System.out.print("자바를\n");
+              System.out.println("배웁니다. ");
+
+
+              // 추가 예제
+            System.out.println("\n름에서 성만 출력하기");
+            String value = "홍길동";
+            char word = value.charAt(0);
+            System.out.println(word);
+        }
+}
