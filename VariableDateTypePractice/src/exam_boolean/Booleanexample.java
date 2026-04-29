7f406a2c4e8a0201faa1fab2a8bc214edf56499d
gustmdakstp <hs8206@naver.com>
2026-04-29 14:12:50 +0900

    연습용1

---
+++ VariableDateTypePractice/src/exam_boolean/Booleanexample.java
@@ -0,0 +1,19 @@
+package exam_boolean;
+
+public class Booleanexample {
+        public static void main(String[] args) {
+            // 상태값에 따라 조건문과 제어문의 실행 흐름을 변경하는데 사용
+            boolean stop = true;
+            if (stop) {
+                System.out.println("중지합니다. ");
+            } else {
+                System.out.println("시작합니다. ");
+            }
+
+            int x = 10;
+            boolean result = (x == 20); //변수 x의값이 20인가?
+            boolean result2 = (x != 20); //변수 x의값이 20이 아닌가?
+            System.out.println("result1: " + result1);
+            System.out.println("result2: " + result2);
+        }
+}
