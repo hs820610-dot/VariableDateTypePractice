2baebb39af924598691078ff5af5b367ac1f6636
gustmdakstp <hs8206@naver.com>
2026-04-30 16:42:17 +0900

    Scanner 사용연습본

---
+++ VariableDateTypePractice/src/test/Example3.java
@@ -0,0 +1,22 @@
+package test;
+
+import java.util.Scanner;
+
+public class Example3 {
+    public static void main(String[] args) {
+
+        int egg;
+        int a, b;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("계란의 수");
+        input.nextInt();
+
+        a = 12;
+
+
+
+
+    }
+}
